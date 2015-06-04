cf8287153875a9c079d4f188b5bf157ba94996b5
Richard Smith-Unna <richardsmith404@gmail.com>
2015-06-04 23:49:14 +0100

    Address PR review comments

--- salmon.rb
+++ salmon.rb
@@ -1,16 +1,20 @@
 class Salmon < Formula
-  homepage "http://salmon.readthedocs.org/en/latest/"
+  homepage "https://github.com/COMBINE-lab/salmon"
   # tag "bioinformatics"
 
   url "https://github.com/COMBINE-lab/salmon/archive/v0.4.0.tar.gz"
-  sha1 "3552ab0e8bb85be56b12f1b34356daaf48a43469"
+  sha256 "bbeebdaed11e0b79c32bc6620617749d89e116bf1312e75751a78e438a17fb27"
   head "https://github.com/COMBINE-lab/salmon.git", :branch => "master"
+
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "52e52d191915334cc33f7613fdcce3e0b4d71d1f" => :yosemite
+    sha1 "a04b4b5eadde9c0e93c1fc6253ea8af6f3d1cb6a" => :mavericks
+  end
 
   # See https://github.com/kingsfordgroup/sailfish/issues/74
   needs :cxx11
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "tbb"
