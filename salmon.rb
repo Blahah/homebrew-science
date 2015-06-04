7ea561e4c30ca40b5475aa39f3dcff279db3ee8b
Richard Smith-Unna <rds45@cam.ac.uk>
2015-06-04 23:26:22 +0100

    Update Salmon to 0.4.0

--- salmon.rb
+++ salmon.rb
@@ -1,16 +1,10 @@
 class Salmon < Formula
-  homepage "http://www.cs.cmu.edu/~ckingsf/software/sailfish/downloads.html"
+  homepage "http://salmon.readthedocs.org/en/latest/"
   # tag "bioinformatics"
 
-  url "https://github.com/kingsfordgroup/sailfish/archive/v0.3.0.tar.gz"
-  sha1 "69f5752d79b3d8c8ba477ac8ba8d96350a68ebc4"
-  head "https://github.com/kingsfordgroup/sailfish.git", :branch => "develop"
-
-  bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    sha1 "52e52d191915334cc33f7613fdcce3e0b4d71d1f" => :yosemite
-    sha1 "a04b4b5eadde9c0e93c1fc6253ea8af6f3d1cb6a" => :mavericks
-  end
+  url "https://github.com/COMBINE-lab/salmon/archive/v0.4.0.tar.gz"
+  sha1 "3552ab0e8bb85be56b12f1b34356daaf48a43469"
+  head "https://github.com/COMBINE-lab/salmon.git", :branch => "master"
 
   # See https://github.com/kingsfordgroup/sailfish/issues/74
   needs :cxx11
